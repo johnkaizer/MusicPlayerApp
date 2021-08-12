76ad4c4f844dc7bd5bf37ad83e8b5309a21ea89d
john kaizer <johnkaiser964@gmail.com>
2021-08-12 10:43:32 +0300

    Text Adapter and ItemView

--- app/src/main/java/com/example/coolmusicplayer/HomeActivity.java
+++ app/src/main/java/com/example/coolmusicplayer/HomeActivity.java
@@ -11,8 +11,17 @@ import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.security.Permissions;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HomeActivity extends AppCompatActivity implements Permission {
@@ -51,6 +60,8 @@ public class HomeActivity extends AppCompatActivity implements Permission {
         }
     }
 
+    private boolean isCoolMusicPlayerInit;
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -58,7 +69,55 @@ public class HomeActivity extends AppCompatActivity implements Permission {
             requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
             return;
         }
+        if (isCoolMusicPlayerInit) {
+            final ListView listView = findViewById(R.id.listView);
+            isCoolMusicPlayerInit = true;
+        }
     }
+    class textAdapter extends BaseAdapter{
+        private List<String> data = new ArrayList<>();
+        void  setData(List<String> mData){
+            data.clear();
+            data.addAll(mData);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null){
+                convertView = LayoutInflater.from(parent.getContext()).
+                        inflate(R.layout.item, parent, false);
+                convertView.setTag(new viewHolder((TextView)convertView.findViewById(R.id.myItem)));
+
+            }
+            viewHolder holder = (viewHolder) convertView.getTag();
+            final String item = data.get(position);
+            holder.info.setText(item.substring(item.lastIndexOf('/')+1));
+            return convertView;
+        }
+        class  viewHolder{
+            TextView info;
+            viewHolder(TextView mInfo){
+                info = mInfo;
+            }
+        }
+    }
+
 }
 
 
